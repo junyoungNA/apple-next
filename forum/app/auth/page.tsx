9088c4c8fa47bf5350b3944e681d917800aa6319
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-06-25 20:50:35 +0900

    jwt사용

--- forum/app/auth/page.tsx
+++ forum/app/auth/page.tsx
@@ -4,11 +4,13 @@ const Auth: NextPage = async () => {
   return (
     <div>
       <h4>회원가입</h4>
-      <form action={"/api/auth"} method="POST">
+      <form action={"/api/auth/signup"} method="POST">
         <label>아이디</label>
-        <input type="text" name="userId" maxLength={20} />
+        <input type="text" name="userId" maxLength={20}  placeholder="아이디"/>
         <label>비밀번호</label>
-        <input type="password" name="password" maxLength={15} />
+        <input type="password" name="password" maxLength={15} placeholder="비밀번호" />
+        <label>이메일</label>
+        <input type="email" name="email" maxLength={15} placeholder="이메일" />
         <button type="submit">확인</button>
       </form>
     </div>
---
+++ forum/pages/api/auth/signup.ts
@@ -0,0 +1,29 @@
+import { insertUser, findUserList } from "@/app/util/mongo";
+import { ReqRes } from "../test";
+import bcrypt from 'bcrypt';
+
+const signupHandler : ReqRes  = async(req, res) => {
+    if(req.method ==='POST') {
+        if (req.body.userId.trim() === "" || req.body.password.trim() === "") {
+            return res.status(500).json("공백은 입력할 수 없습니다.");
+        }
+        const result = await findUserList();
+        const find = result.findIndex(item => {
+        if ("email" in item) {
+            return req.body.email === item.email;
+        }
+        return false;
+        });
+        if (find !== -1) {
+            console.log("이미 가입된 이메일입니다.");
+            return res.redirect(302, "/auth");
+        }
+        const hash = await bcrypt.hash(req.body.password,10);
+        console.log(hash,'hash');
+        req.body.password = hash;
+        await insertUser(req.body);
+        res.status(200).json('가입성공');
+    }
+}
+
+export default signupHandler;
